2625bbb3cddd0874a1ad2cfd424d6da752b827c3
Robert Marko <robert.marko@sartura.hr>
2025-06-08 20:28:12 +0200

    genimage: update to current git HEAD
    genimage releases are rather rare and we need features that were added
    after the last v18 release, so use current git HEAD instead.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-GENIMAGE_VERSION = 18
-GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
-GENIMAGE_SITE = https://github.com/pengutronix/genimage/releases/download/v$(GENIMAGE_VERSION)
+GENIMAGE_VERSION = d816d0d0d39ce9dd951d7c426fb7447dff6dd8d4
+GENIMAGE_SITE = https://github.com/pengutronix/genimage.git
+GENIMAGE_SITE_METHOD = git
 HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse
 GENIMAGE_LICENSE = GPL-2.0
 GENIMAGE_LICENSE_FILES = COPYING
+
+GENIMAGE_AUTORECONF = YES
 
 $(eval $(host-autotools-package))
